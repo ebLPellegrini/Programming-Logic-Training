afe6db85c901d30f30bf6d19e8a917b114ad39d6
ebLPellegrini <118392848+ebLPellegrini@users.noreply.github.com>
2025-03-17 20:34:40 -0400

    Update 1172.py

--- Python Training/1-begginer/array-replacement-1/1172.py
+++ Python Training/1-begginer/array-replacement-1/1172.py
@@ -2,13 +2,11 @@ def main():
     array = []
     for _ in range(10):
         array.append(int(input()))
-    
-    count = 0    
-    for item in array:
+        
+    for index, item in enumerate(array):
         if item <= 0:
             item = 1
         
-        print(f'X[{count}] = {item}')
-        count += 1
+        print(f'X[{index}] = {item}')
         
 main()
