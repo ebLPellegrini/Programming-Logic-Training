2c3eeea69ed949586df55502e1fcb7150f4b6b13
ebLPellegrini <118392848+ebLPellegrini@users.noreply.github.com>
2025-09-03 13:29:50 -0400

    import

---
+++ Codevolution Course /Tree Implementation/queueReuse.py
@@ -0,0 +1,30 @@
+class Queue:
+    def __init__(self):
+        self.items = {}
+        self.rear = 0
+        self.front = 0
+    
+    def enqueue(self, element):
+        self.items[self.rear] = element
+        self.rear += 1
+        
+    def dequeue(self):
+        item = self.items[self.front]
+        del self.items[self.front]
+        self.front += 1
+        return item
+    
+    def isEmpty(self):
+        if self.rear - self.front == 0:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        return self.items[self.front]
+    
+    def size(self):
+        return self.rear - self.front
+    
+    def printQueue(self):
+        print(self.items)
